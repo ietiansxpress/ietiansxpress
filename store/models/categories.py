c38d3d39ac7064536210d4c3f7b39a5015b9a2fe
Tushar Shukla <tushar.shukla2222@gmail.com>
2021-10-08 02:49:57 +0530

    first commit

---
+++ store/models/categories.py
@@ -0,0 +1,13 @@
+from django.db  import models
+
+class Category(models.Model):
+    name=models.CharField(max_length=20)
+
+
+    def __str__(self):
+        return self.name
+    
+    def get_all_category():
+        return Category.objects.all()
+    
+    
